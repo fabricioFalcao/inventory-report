8291a9e200fcdad195180f3d63fb62fd4c0df2a1
fabricioFalcao <fabriciohfd@gmail.com>
2024-05-10 12:54:33 -0300

    Requisito 5

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,13 @@
+from typing import Optional, List
+from inventory_report.product import Product
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        self.__data = [] if data is None else data
+
+    @property
+    def data(self) -> List[Product]:
+        return self.__data
+    
+    def add_data (self, data: List[Product]) -> None:
+        self.__data.extend(data)
