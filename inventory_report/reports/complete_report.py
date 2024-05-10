97c95a7c6fe8c49cb3114b56bca93950d3442343
fabricioFalcao <fabriciohfd@gmail.com>
2024-05-10 19:51:19 -0300

    Requisito 9

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        self.report_data()
+
+        stocked_products_output = "\n".join(
+            f"- {company}: {quantity}"
+            for company, quantity in self._stocked_products_by_company.items()
+        )
+
+        return (
+            f"Oldest manufacturing date: {self._oldest_manufacturing_date}\n"
+            f"Closest expiration date: {self._closest_expiration_date}\n"
+            f"Company with the largest inventory: {self._largest_company_inventory}\n"
+            f"Stocked products by company:\n"
+            f"{stocked_products_output}\n"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,18 +5,22 @@ from datetime import datetime
 
 class SimpleReport(Report):
     def __init__(self) -> None:
-        self.__inventories = []
+        self._inventories = []
+        self._oldest_manufacturing_date = None
+        self._closest_expiration_date = None
+        self._stocked_products_by_company = None
+        self._largest_company_inventory = None
 
     def add_inventory(self, inventory: Inventory) -> None:
-        self.__inventories.append(inventory)
+        self._inventories.append(inventory)
 
-    def generate(self) -> str:
+    def report_data(self) -> None:
         today = datetime.today().date()
         oldest_manufacturing_date = today
         closest_expiration_date = datetime(year=3000, month=1, day=1).date()
-        largest_company_inventory = {}
+        company_inventory = {}
 
-        for inventory in self.__inventories:
+        for inventory in self._inventories:
             for product in inventory.data:
                 manufacturing_date = datetime.strptime(
                     product.manufacturing_date, "%Y-%m-%d"
@@ -25,10 +29,10 @@ class SimpleReport(Report):
                     product.expiration_date, "%Y-%m-%d"
                 ).date()
 
-                if product.company_name not in largest_company_inventory:
-                    largest_company_inventory[product.company_name] = 1
+                if product.company_name not in company_inventory:
+                    company_inventory[product.company_name] = 1
                 else:
-                    largest_company_inventory[product.company_name] += 1
+                    company_inventory[product.company_name] += 1
 
                 if manufacturing_date < oldest_manufacturing_date:
                     oldest_manufacturing_date = manufacturing_date
@@ -39,8 +43,21 @@ class SimpleReport(Report):
                 ):
                     closest_expiration_date = expiration_date
 
+        self._oldest_manufacturing_date = oldest_manufacturing_date.strftime(
+            "%Y-%m-%d"
+        )
+        self._closest_expiration_date = closest_expiration_date.strftime(
+            "%Y-%m-%d"
+        )
+        self._stocked_products_by_company = company_inventory
+        self._largest_company_inventory = max(
+            company_inventory.items(), key=lambda x: x[1]
+        )[0]
+
+    def generate(self) -> str:
+        self.report_data()
         return (
-            f"Oldest manufacturing date: {oldest_manufacturing_date.strftime('%Y-%m-%d')}\n"
-            f"Closest expiration date: {closest_expiration_date.strftime('%Y-%m-%d')}\n"
-            f"Company with the largest inventory: {max(largest_company_inventory.items(), key=lambda x: x[1])[0]}\n"
+            f"Oldest manufacturing date: {self._oldest_manufacturing_date}\n"
+            f"Closest expiration date: {self._closest_expiration_date}\n"
+            f"Company with the largest inventory: {self._largest_company_inventory}\n"
         )
