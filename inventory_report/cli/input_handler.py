07470fdd4ae3928090527588423d820742a661b0
fabricioFalcao <fabriciohfd@gmail.com>
2024-05-10 20:25:49 -0300

    Requisito 10

--- inventory_report/cli/input_handler.py
+++ inventory_report/cli/input_handler.py
@@ -1,4 +1,8 @@
 from typing import List
+from inventory_report.importers import JsonImporter, CsvImporter
+from inventory_report.inventory import Inventory
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 def process_report_request(file_paths: List[str], report_type: str) -> str:
@@ -6,4 +10,22 @@ def process_report_request(file_paths: List[str], report_type: str) -> str:
     Process the report given a list of file paths and a report type,
     and returns the result.
     """
-    raise NotImplementedError
+    data = []
+    for file_path in file_paths:
+        if file_path.endswith(".json"):
+            data.extend(JsonImporter(file_path).import_data())
+        elif file_path.endswith(".csv"):
+            data.extend(CsvImporter(file_path).import_data())
+
+    inventory = Inventory(data)
+
+    if report_type == "simple":
+        report = SimpleReport()
+        report.add_inventory(inventory)
+        return report.generate()
+    elif report_type == "complete":
+        report = CompleteReport()
+        report.add_inventory(inventory)
+        return report.generate()
+    else:
+        raise ValueError("Report type is invalid.")
